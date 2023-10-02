234807bb49e0edf7cb8543d89c90afecab0e0944
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-10-02 09:24:47 -0300

    Primeiro teste de envio de arquivos utilizando  o FILE

--- Upload de arquivos com Amazon S3/backend/src/main/java/com/upload/arquivos/amazon/jefferson/JeffersonApplication.java
+++ Upload de arquivos com Amazon S3/backend/src/main/java/com/upload/arquivos/amazon/jefferson/JeffersonApplication.java
@@ -1,13 +1,25 @@
 package com.upload.arquivos.amazon.jefferson;
 
+import com.upload.arquivos.amazon.jefferson.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JeffersonApplication {
+public class JeffersonApplication implements CommandLineRunner {
+
+	@Autowired
+	S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JeffersonApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		s3Service.uploadFile("C://Users//jeffe//Downloads//2023-09-20_08h51_52.png");
+
+	}
 }
