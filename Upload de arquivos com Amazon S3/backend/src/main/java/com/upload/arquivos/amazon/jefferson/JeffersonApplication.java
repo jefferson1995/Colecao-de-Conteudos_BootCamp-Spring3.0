459eb0dc9224cc4d3a7fe41b2db52c8365a6fd65
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-10-02 08:10:49 -0300

    Criado projeto para enviar arquivos de testes

---
+++ Upload de arquivos com Amazon S3/backend/src/main/java/com/upload/arquivos/amazon/jefferson/JeffersonApplication.java
@@ -0,0 +1,13 @@
+package com.upload.arquivos.amazon.jefferson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffersonApplication.class, args);
+	}
+
+}
---
+++ Upload de arquivos com Amazon S3/backend/src/test/java/com/upload/arquivos/amazon/jefferson/JeffersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload.arquivos.amazon.jefferson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
