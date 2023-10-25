eecf20470829021c63e402ee30a50f59b7277f45
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-10-25 11:27:22 -0300

    projeto envio de notificação backend criado

---
+++ Push notifications com Firebase/backend/src/main/java/br/com/jefferson/notificacaofirebase/NotificacaofirebaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.notificacaofirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaofirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaofirebaseApplication.class, args);
+	}
+
+}
---
+++ Push notifications com Firebase/backend/src/test/java/br/com/jefferson/notificacaofirebase/NotificacaofirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.notificacaofirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaofirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
